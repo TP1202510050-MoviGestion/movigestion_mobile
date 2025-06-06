4f5767db2776e9fac48588d95d00b30da4c29c08
ELÍAS TORRES ESPINOZA <pierotarazona633@gmail.com>
2025-06-05 20:35:05 -0500

    feat: implement mobile aplication

---
+++ lib/core/app_constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  static const String baseUrl = 'https://app-241107014459.azurewebsites.net';
+
+  // Rutas de API
+  static const String profile = '/api/profiles';
+  static const String report = '/api/reports';
+  static const String shipment = '/api/shipments';
+  static const String vehicle = '/api/vehicles';
+}
